c86424b197fcebe125708f52bcb7c04fb4cf9bc8
Nazrul Islam Nayan <nazrulislamnayan7@gmail.com>
2022-07-08 11:14:40 +0600

    init plugin

---
+++ eux-crm-manager.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: EUX CRM Manager
+ * Plugin URI: https://profiles.wordpress.org/wpfeelteam/
+ * Description: A CRM Manager for 3CX with Woocommerce API Integration.
+ * Version: 1.0.0
+ * Author: EUX Digital Agency
+ * Author URI: https://eux.com.au/
+ * Text Domain: eux-crm-manager
+ * Domain Path: /i18n/languages/
+ *
+ * WP Requirement & Test
+ * Requires at least: 4.4
+ * Tested up to: 6.0
+ * Requires PHP: 5.6
+ *
+ * WC Requirement & Test
+ * WC requires at least: 3.2
+ * WC tested up to: 6.3
+ *
+ * @package EUX_CRM_Manager
+ */
+
+defined( 'ABSPATH' ) || exit;
