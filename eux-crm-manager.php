f8695794d93422324b876240a399ac88bbedebdb
Nazrul Islam Nayan <nazrulislamnayan7@gmail.com>
2022-07-08 23:36:43 +0600

    added response

--- eux-crm-manager.php
+++ eux-crm-manager.php
@@ -22,3 +22,58 @@
  */
 
 defined( 'ABSPATH' ) || exit;
+
+// autoload
+require __DIR__ . '/vendor/autoload.php';
+
+if ( ! defined( 'EUX_FILE' ) ) {
+    define( 'EUX_FILE', __FILE__ );
+}
+
+if ( ! defined( 'EUX_PLUGIN_URL' ) ) {
+    define( 'EUX_PLUGIN_URL', plugins_url( '', EUX_FILE ) );
+}
+
+if ( ! defined( 'EUX_PLUGIN_PATH' ) ) {
+    define( 'EUX_PLUGIN_PATH', untrailingslashit(plugin_dir_path(EUX_FILE)) );
+}
+
+include EUX_PLUGIN_PATH . '/includes/class-eux-crm-api.php';
+$eux_crm_api = EUX_CRM_API::instance();
+$eux_crm_api->init_rest_api();
+
+
+add_filter('woocommerce_rest_prepare_customer', 'filter_response', 10, 3);
+function filter_response($response, $user_data, $request) {
+    // Customize response data here
+    $user_id = $user_data->ID;
+
+    $phone = $request->get_param('phone');
+
+    if( isset($phone) && ! empty($phone) ) {
+        $user_id = '';
+        global $wpdb;
+        $search_phone = (string) "%" . $phone . "%";
+        $query = "SELECT * FROM $wpdb->usermeta WHERE $wpdb->usermeta.meta_key = 'billing_phone' AND $wpdb->usermeta.meta_value LIKE '" . $search_phone . "'  ORDER BY $wpdb->usermeta.user_id DESC";
+
+        $customer = $wpdb->get_results($query, OBJECT);
+
+        if( isset($customer) && count($customer) > 0 ) {
+            $user_id = $customer[0]->user_id;
+        }
+
+        $consumer_key = $request->get_param('oauth_consumer_key');
+
+        //cs_29dd07d7098fb1c94605d8cd47dae2db50a28ef2
+        $table_name = $wpdb->prefix . "woocommerce_api_keys";
+        $query = "SELECT 'consumer_secret' FROM $table_name WHERE 'user_id' = $user_id";
+        $customer = $wpdb->get_results($query, OBJECT);
+        $consumer_secret_key = $request->get_param('oauth_consumer_key');
+
+
+    }
+//
+//    die(print_r($response->data));
+
+    return $response;
+}
---
+++ includes/class-eux-crm-api.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * EUX CRM API
+ *
+ * @package    EUX_CRM_API
+ * @subpackage EUX_CRM_API
+ * @author     EUX Digital Agency <https://eux.com.au/>
+ */
+
+defined('ABSPATH') || exit;
+
+use Automattic\WooCommerce\Client;
+
+if ( ! class_exists('EUX_CRM_API') ) {
+    class EUX_CRM_API {
+
+        public $customer_data;
+
+        protected static $_instance = null;
+
+        function __construct() {
+            do_action( 'eux_crm_api_loaded', $this );
+        }
+
+        // instance
+        public static function instance() {
+            if ( is_null( self::$_instance ) ) {
+                self::$_instance = new self();
+            }
+
+            return self::$_instance;
+        }
+
+        // authorize
+        function authorize() {
+
+            $woocommerce = new Client(
+                'http://localhost:10033/',
+                'ck_2f7573d408d421a348a2da3e9169745c14048a38',
+                'cs_29dd07d7098fb1c94605d8cd47dae2db50a28ef2',
+                [
+                    'version' => 'wc/v3',
+                ]
+            );
+
+
+        }
+
+        // init rest api
+        function init_rest_api() {
+            add_action('rest_api_init',  [$this, 'eux_register_api']);
+        }
+
+        // create eux api endpoint
+        function eux_register_api() {
+            //(?P<phone>[a-zA-Z]+)
+            //(?P<phone>[a-zA-Z0-9-]+)
+
+            // register endpoint to get icon list
+            register_rest_route(
+                'wc/v3',
+                '/posts/',
+                array(
+                    'methods' => 'GET',
+                    'callback' => [$this, 'eux_get_customer_by_phone'],
+                    'permission_callback' => '__return_true',
+                )
+            );
+        }
+
+        // get customer data by phone number
+        function eux_get_customer_by_phone($request) {
+            $result  = [];
+            $phone = $request->get_param('phone');
+
+
+
+            die( print_r($request) );
+
+
+
+            return new WP_REST_Response([
+                $phone
+            ]);
+        }
+    }
+}
